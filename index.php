5cfcdf01d6e0d55c061f81ebc8f5f8ad9d3faef1
davidmartianus.simarmata@gmail.com <davidmartianus.simarmata@gmail.com>
2022-07-17 10:48:14 +0700

    validate initial letter v0.1

--- index.php
+++ index.php
@@ -1,7 +1,23 @@
 <html>
 <body>
 
+<?php 
+$name_substr = strtolower(substr($_POST["name"],0,1));
+$vocal_ltr = ["a", "i", "u", "e", "o"];
+?>
+
 Welcome <?php echo $_POST["name"]; ?><br>
+
+<?php
+    if (in_array($name_substr, $vocal_ltr)) {
+        echo "Nama Anda berawalan dari huruf vokal";
+    }else{
+        echo "Nama Anda berawalan bukan dari huruf vokal";
+    }
+?>
+
+<br>
+
 Your email address is: <?php echo $_POST["email"]; ?>
 
 </body>
